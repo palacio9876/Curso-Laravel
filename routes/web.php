0e5f8eb522f14521902066fe150b657188fd1950
Cristian Palacio <cristiancamilopala@gmail.com>
2024-01-09 08:12:41 -0500

    Learned: Views, language, routes, fomrs and migrate

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Chirp;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'welcome')->name('Welcome');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::view('/dashboard','dashboard')->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', function () {
+        return view('chirps.index');
+    })->name('chirps.index');
+
+    Route::post('/chirps', function (){
+        Chirp::create([
+            'message' => request('message'),
+            'user_id' => auth()->id(),
+        ]);
+
+        return to_route('chirps.index')->with('status', __('Chirp created successfully'));
+    });
 });
+
+require __DIR__.'/auth.php';
+
+// Route::get('/chirps/{chirp}', function ($chirp) { //nombre de la ruta con su parametro 
+//     if($chirp === "2"){
+//         return to_route('chirps.index'); //Se utiliza el "to_route" y el nombre de la ruta sin necesidad de cambiar manualmente cada vez que el nombre de la ruta cambie
+//     }
+//         return "Welcome to chirps ". $chirp;//Devolvemos la view con el parametro dado por la url
+// });
